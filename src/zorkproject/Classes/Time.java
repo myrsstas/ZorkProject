a2ae3e677fb406f0a3cd0cae3c42065ef401661e
paulkokos <paulkokos@yahoo.gr>
2017-03-29 07:12:49 +0300

    Corrections in UI

--- src/zorkproject/Classes/Time.java
+++ src/zorkproject/Classes/Time.java
@@ -1,8 +1,16 @@
 package zorkproject.Classes;
 
+import java.util.Date;
+
 /**
  * Created by paulkokos on 29/3/2017.
  *
  */
 public class Time {
+    private final Date createdDate = new java.util.Date();
+
+    public int getAgeInSeconds() {
+        java.util.Date now = new java.util.Date();
+        return (int) ((now.getTime() - this.createdDate.getTime()) / 1000);
+    }
 }
